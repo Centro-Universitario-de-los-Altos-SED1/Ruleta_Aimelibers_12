dfa61972e5bb07c5cea7247589bbc426c6e27e48
Juan7634 <68211985+Juan7634@users.noreply.github.com>
2020-09-29 22:50:39 -0500

    Se corrigio el metodo de cargar datos y se impelentos otro metodo para guardar la ronda
    Se sigue mejorando el codigo y arreglando los errores

--- Ejercicio1/src/ejercicio1/Ejercicio1.java
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -1,14 +1,14 @@
-/*
-Arreglo de Objetos
- */
-package ejercicio1;
-
+import java.awt.HeadlessException;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.StringTokenizer;
+
 
 
 
@@ -18,19 +18,23 @@ import java.util.StringTokenizer;
  *
  * @author Aimelibers
  */
-public class Ejercicio1 {
+public class Ejercicio_1 {
     
-     private int contador = -1, tope = 22;
-     private Datos alumnos[] = new Datos[tope];
+     private int contador = 0, tope = 22;
      private ArrayList<Integer> numbers = new ArrayList<>(tope);
+     String dato_1[][]=new String[22][3];
      
+     public Ejercicio_1(){
+         
+     }
      
         /**
      * Método para cargar el Archivo con la información de la tabla
      * @param archivo File
+     * @param alumnos
      * @return booleano, falso= hubo un error, true= cargado correctamente
      */
-    protected boolean cargarArchivo(File archivo){
+    protected String[][] cargarArchivo(File archivo,Datos alumnos[]){
         
         /**JFileChooser fc = new JFileChooser();
         fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
@@ -43,33 +47,56 @@ public class Ejercicio1 {
         File archivo = fc.getSelectedFile(); //A la variable archivo se le asigna el archivo seleccionado
         **/
         int contador_carga=0;
+        
         try { //Por si hay errores no se detenga el programa
             FileReader fr = new FileReader(archivo); 
             BufferedReader bf = new BufferedReader(fr); //Cargar todo en memoria
+            System.out.println("1");
             String linea;
             linea = bf.readLine(); //Cada línea se asigna a la cadena
             String[] lista = new String [3]; //Arreglo para cargar cada línea de la tabla
+            System.out.println("1");
             while (linea!=null){ //Repetir hasta que las filas sean nulas
                 StringTokenizer st = new StringTokenizer(linea, ","); //Porque cada columna está dividida por coma
                  lista[0]= st.nextToken();
                 lista[1]= st.nextToken();
-                
-                alumnos[contador_carga].setNombre(lista[0]);
-		alumnos[contador_carga].setNumeroDeLista(Integer.parseInt(lista[1]));
-		alumnos[contador_carga].setControl(false);
+                lista[2]=st.nextToken();
+                dato_1[contador_carga][0]=lista[0];
+                dato_1[contador_carga][1]=lista[1];
+                dato_1[contador_carga][2]=lista[2];
+               
 		contador_carga++;
                 linea = bf.readLine();
                 
             }
             bf.close();
-            return true;
-        } catch (Exception e) { //Si hubo errores manda mensaje
-            return false; 
+            return dato_1;
+        } catch (IOException | NumberFormatException e) { //Si hubo errores manda mensaje
+            
+            return null;
         }
+         
+    }
+    public boolean guardar_datos(File archivo,String datos[][]){
+        
+    try{//Metodo utilizado por si hay algun error en el proceso
+        FileWriter fw=new FileWriter(archivo);//Se instancia un objeto para una lectura de archivo
+        PrintWriter pw=new PrintWriter(fw);//Y lo escribe, el como lo va a a escribir
+            for(int i=0;i<22;i++){//Se va a leer todos los datos para poder leer el archivo
+                for(int x=0;x<3;x++){
+                    pw.print(datos[i][x]+",");//Lo escribe y lo separa por comas
+                }
+                pw.println();//Salto de fila
+            }
+            pw.close();//Se cierra la escritura
+            return true;
+    }catch(HeadlessException e){
+        return false;
+    }
     }
     
-      public int elegir_al_azar(){
-        while(alumnos[numbers.get(contador)].isControl()==true){
+      public String elegir_al_azar(Datos alumnos[]){
+          while(alumnos[numbers.get(contador)].isControl()==true){
               contador++;
         if(contador==tope){
             Collections.shuffle(numbers);
@@ -82,7 +109,8 @@ public class Ejercicio1 {
           }
           alumnos[numbers.get(contador)].setControl(true);
         
-        return alumnos[numbers.get(contador)].numeroDeLista;
+        return alumnos[numbers.get(contador)].getNombre();
+          
     }
     
     /**
@@ -94,7 +122,8 @@ public class Ejercicio1 {
                 numbers.add(i);
         }
         Collections.shuffle(numbers);
-    }    
+    }
+    
     
     
     
